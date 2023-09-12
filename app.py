530bf61182f4aea902707c8acafb81430e0cf819
ghjk <ghjk>
2023-09-12 19:34:46 +0300

    v1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/delta/OneDrive/Рабочий стол/Учёба 407/Кочетков/lab1/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/delta/OneDrive/Рабочий стол/Учёба 407/Кочетков/lab3/database.db'
 db = SQLAlchemy(app)
 app.secret_key = 'secret_key'
 
@@ -30,29 +32,34 @@ def register():
         if existing_user:
             return "Пользователь с таким именем уже существует."
         
-        new_user = User(username=username, password=password)
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        new_user = User(username=username, password=hashed_password.decode('utf-8'))
         db.session.add(new_user)
-        db.session.commit() 
+        db.session.commit()
         return redirect('/main')
 
     return render_template('register.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if 'username' in session:
         return redirect('/main')
-    
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
         user = User.query.filter_by(username=username).first()
-        
-        if user and user.check_password(password):
+
+        if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
             session['username'] = user.username
             return redirect('/main')
-    
+
     return render_template('login.html')
+
 
 @app.route('/main')
 def main():
